2f1cbb494507e17e4445d8fc86c6df99f03b517d
Unknown <stajun@naver.com>
2019-01-16 11:43:16 +0900

    주석추가

--- src/main/java/kr/green/spring/HomeController.java
+++ src/main/java/kr/green/spring/HomeController.java
@@ -17,27 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		/* 컨트롤러에서 jsp로 데이터를 전달하기 위해서
+		 * 1. 매개변수에 Model 객체를 추가 : Model model
+		 * 2. model.addAttribute()를 통해 데이터 전달
+		 *    model.addAttribute("jsp에서받는이름",전달할데이터);*/
+		model.addAttribute("serverTime", "서버시간" );
 		return "home";
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String homePost(String name) {
+		/* jsp에서 넘겨준 데이터를 받기 위해서는 태그의 속성 name과 일치하는
+		 * 이름을 매개변수의 이름으로 설정한다. */
 		System.out.println("jsp에서 넘어온 값 : "+name);
 		return "redirect:/";
 	}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -9,9 +9,11 @@
 <h1>
 	Hello world!  
 </h1>
-
+<%-- 컨트롤러에서 전달한 데이터를 받기위해 ${컨트롤러에서전달한이름}을 사용한다. --%>
 <P>  The time on the server is ${serverTime}. </P>
-<form method="post">
+<!-- jsp에서 컨트롤러로 데이터를 전달하려면 form태그를 이용 -->
+<form method="post" action="<%=request.getContextPath() %>/">
+	<!-- 전달하려는 데이터가 있는 태그의 속성 name을 입력한다. -->
 	<input type="text" name="name" >
 	<button>제출</button>
 </form>
